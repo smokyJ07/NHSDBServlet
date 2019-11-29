0478e083adadd6014ce62ad5ea6769009e17a7c2
“alejandra-gutierrez” <ag1917@ic.ac.uk>
2019-11-29 15:50:02 +0000

    Added method that creates connection as a global variable. Still needs to print to console.

--- src/main/java/DBServlet.java
+++ src/main/java/DBServlet.java
@@ -1,26 +1,49 @@
-
 import com.google.gson.Gson;
-
-import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.stream.Collectors;
+import java.sql.Statement;
 
 @WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
 public class DBServlet extends HttpServlet {
+    static Statement s = null;
+
+    protected static void initDatabaseConnection() {
+        String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(dbUrl, "postgres", "aleseb99");
+            s = conn.createStatement();
+        }catch (Exception e) {
+            System.out.println(e);
+            String l=e.getMessage();
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         String message = "Hello, World!";
+
         resp.getWriter().write(message);
+
+        ResultSet rset = null;
+        try {
+            rset = getPatient("Gutierrez");
+            String message1 = "Hello, World!";
+            resp.getWriter().write(message1);
+            while(rset.next()){
+                resp.getWriter().write(rset.getInt("id")+" "+ rset.getString("lastName"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -29,66 +52,39 @@ public class DBServlet extends HttpServlet {
         Gson gson = new Gson();
         Patient p=gson.fromJson(reqBody,Patient.class);
         System.out.println(reqBody);
-
     }
 
-    public void createDB() {
-        String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+    public static void addPatient(String name, String lastname, String phonenumber) throws SQLException {
+        initDatabaseConnection();
+        String message;
+        message = "INSERT INTO patients (\"firstName\", \"lastName\", phonenumber) values ('"+name+"', '"+lastname+"','"+phonenumber+"');";
+        s.execute(message);
+    }
 
-        try {
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection(dbUrl, "postgres", "aleseb99");
+    public static void addDoctor(String name, String lastname,String pagernumber) throws SQLException {
+        initDatabaseConnection();
+        String message;
+        message = "INSERT INTO doctors (\"firstName\", \"lastName\", pagernumber) values ('"+name+"', '"+lastname+"','"+pagernumber+"');";
+        s.execute(message);
+    }
 
-            Statement s=conn.createStatement();
+    public static ResultSet getPatient(String lastname) throws SQLException {
+        initDatabaseConnection();
+        String message;
+        message = "select * from patients where \"lastName\" = '"+lastname+"';";
+        s.execute(message);
 
-            String sqlStr;
-            // sqlStr = addPatient("Cristian", "Gutierrez", "222222222");
-            // s.execute(sqlStr);
-
-            sqlStr = addDoctor("Angela", "Kedgley", "11111111");
-            s.execute(sqlStr);
-
-            //String sqlStr = getPatient("Gutierrez");
-            // ResultSet rset=s.executeQuery(sqlStr);
-            // while(rset.next()){
-            //     System.out.println(rset.getInt("id")+" "+ rset.getString("name")+" "+ rset.getString("lastname"));
-            //  }
-            //(int id, String parameter, String newvalue){
-            sqlStr = modPatient(1, "lastname", "Gutierrez");
-            s.execute(sqlStr);
-
-            //  rset.close();
-            s.close();
-            conn.close();
-
-        } catch (Exception e) {
-            String l=e.getMessage();
+        ResultSet rset=s.executeQuery(message);
+        while(rset.next()){
+            System.out.println(rset.getInt("id")+" "+ rset.getString("lastName"));
         }
+        return rset;
     }
 
-    public static String addPatient(String name, String lastname,String phonenumber) {
-        String message;
-        message = "INSERT INTO patients (name,lastname, phonenumber) values ('"+name+"', '"+lastname+"','"+phonenumber+"');";
-        return message;
-    }
-
-    public static String addDoctor(String name, String lastname,String pagernumber) {
-        String message;
-        message = "INSERT INTO doctors (name,lastname, pagernumber) values ('"+name+"', '"+lastname+"','"+pagernumber+"');";
-        return message;
-    }
-
-    public static String getPatient(String lastname) {
-        String message;
-        message = "select * from patients where lastname = '"+lastname+"';";
-        return message;
-    }
-
-    public static String modPatient(int id, String parameter, String newvalue){
+    public static void modPatient(int id, String parameter, String newvalue) throws SQLException {
+        initDatabaseConnection();
         String message;
         message = "update patients set "+parameter+"='"+newvalue+"' where id="+id+";";
-        System.out.println(message);
-        return message;
+        s.execute(message);
     }
-
 }
