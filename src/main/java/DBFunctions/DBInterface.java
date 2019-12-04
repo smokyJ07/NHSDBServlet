aadde5c83f5a7389390307fe880b2357b496863a
“alejandra-gutierrez” <ag1917@ic.ac.uk>
2019-12-04 15:09:28 +0000

    Full pipeline - client to servlet to database - for addPatient method

--- src/main/java/DBFunctions/DBInterface.java
+++ src/main/java/DBFunctions/DBInterface.java
@@ -5,10 +5,28 @@ import com.google.gson.Gson;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class DBInterface {
-    public DBInterface(){
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class DBInterface {
+
+    static Statement s = null;
+
+    public DBInterface(){
+        String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(dbUrl, "postgres", "aleseb99");
+            s = conn.createStatement();
+        }catch (Exception e) {
+            System.out.println(e);
+            String l=e.getMessage();
+        }
     }
+
 
     public void addPatient(JSONObject data){
         try {
@@ -17,8 +35,14 @@ public class DBInterface {
             Patient p = gson.fromJson(patientData, Patient.class);
             System.out.println(patientData);
 
+            String message;
+            message = "INSERT INTO patients (\"firstName\", \"lastName\", phonenumber) values ('"+p.firstName+"', '"+p.lastName+"','"+p.phoneNum+"');";
+            s.execute(message);
+
         }catch(JSONException e){
-            System.out.println("Error while executing addPatient function");
+            System.out.println("Error while executing JSON function in AddPatient");
+        }catch(SQLException e){
+            System.out.println("Error while executing SQL function in Patient");
         }
     }
 }
