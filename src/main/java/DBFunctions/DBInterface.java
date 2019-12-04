0a2e155d34ac329278a325aac5c94d80ed8e5007
“alejandra-gutierrez” <ag1917@ic.ac.uk>
2019-12-04 14:57:09 +0000

    Random error commit

---
+++ src/main/java/DBFunctions/DBInterface.java
@@ -0,0 +1,44 @@
+package DBFunctions;
+
+import DBClasses.Patient;
+import com.google.gson.Gson;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBInterface {
+    static Statement s = null;
+    public DBInterface(){
+        String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(dbUrl, "postgres", "aleseb99");
+            s = conn.createStatement();
+        }catch (Exception e) {
+            System.out.println(e);
+            String l=e.getMessage();
+        }
+    }
+
+    public void addPatient(JSONObject data){
+        try {
+            Gson gson = new Gson();
+            String patientData = data.get("patient").toString();
+            Patient p = gson.fromJson(patientData, Patient.class);
+            System.out.println(patientData);
+
+            String message = "INSERT INTO patients (\"firstName\", \"lastName\", \"phonenumber\") values ('"+p.firstName+"', '"+p.lastName+"','"+p.phoneNum+"');";
+            s.execute(message);
+
+        }catch(JSONException f){
+            System.out.println("Error while executing JSON command function");
+        }catch(SQLException e){
+            System.out.println("Error while executing SQL command function");
+        }
+    }
+}
