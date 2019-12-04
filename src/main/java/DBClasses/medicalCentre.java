23e4cea1d6872b98cd7371eef7b45ae17fa494de
“alejandra-gutierrez” <ag1917@ic.ac.uk>
2019-12-04 16:59:18 +0000

    Full pipeline - client to servlet to database - for addPatient method

---
+++ src/main/java/DBClasses/medicalCentre.java
@@ -0,0 +1,13 @@
+package DBClasses;
+
+public class medicalCentre {
+    public String name;
+    public String address;
+
+
+    public medicalCentre(String name, String address){
+        this.name=name;
+        this.address=address;
+
+    }
+}
